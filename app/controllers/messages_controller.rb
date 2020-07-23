d8ef49962e2e88fa45dba9dd18b93e03ece96a1a
tomohiro441 <metype441wetype@gmail.com>
2020-07-23 21:14:21 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,6 +20,5 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
-  end
-
+  
 end
